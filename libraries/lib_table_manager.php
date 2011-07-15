9735debad9cf5a5c9b70c971a97d76732d4cf8a7
Puji Prayitno <pw.prayitno@gmail.com>
2011-07-15 13:55:05 +0700

    table manager library

---
+++ libraries/lib_table_manager.php
@@ -0,0 +1,40 @@
+<?php
+class Lib_table_manager{
+public $obj;
+	 function __construct(){
+		// parent::__construct();
+		$this->obj = & get_instance();
+		$this->obj->load->library('table');
+	 }
+	function create_table($rows){
+		foreach($rows as $value){
+			$this->obj->table->add_row($value);
+		}
+		echo $this->obj->table->generate();
+	}
+	function create_table_template($tbl_id){
+		$tmpl = array (
+			'table_open'          => '<table border="0" cellpadding="4" cellspacing="0" id="' . $tbl_id . '">',
+
+			'heading_row_start'   => '<tr>',
+			'heading_row_end'     => '</tr>',
+			'heading_cell_start'  => '<th>',
+			'heading_cell_end'    => '</th>',
+
+			'row_start'           => '<tr>',
+			'row_end'             => '</tr>',
+			'cell_start'          => '<td>',
+			'cell_end'            => '</td>',
+
+			'row_alt_start'       => '<tr>',
+			'row_alt_end'         => '</tr>',
+			'cell_alt_start'      => '<td>',
+			'cell_alt_end'        => '</td>',
+
+			'table_close'         => '</table>'
+		);
+		return $tmpl;
+
+	}
+
+}
